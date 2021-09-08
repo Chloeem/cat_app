fbfade0bbaccc0340b69fa7702e8efaecb07298b
Chloe <chloeemstokes@gmail.com>
2021-09-08 13:10:45 +0100

    Added params to the app build

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get "/another" do
   "Yes its another route"
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_name = %w(Amigo Misty Almond).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
